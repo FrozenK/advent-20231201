5a6576c81e3392b758c72e3a1fe10c91b3121750
Pascal Filipovicz <pascal.filipovicz@frozenk.net>
2023-12-01 15:40:45 +0100

    step 1

--- main.go
+++ main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("GO GO GO")
+	sum := 0
+
+	fmt.Println("Hello")
+	f, err := os.Open("input.txt")
+	check(err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		num := 0
+
+		l := len(line)
+		for i := 0; i < l; i++ {
+			val, err := strconv.Atoi(string(line[i]))
+			if err != nil {
+				continue
+			}
+			num += val * 10
+			break
+		}
+		for i := l - 1; i >= 0; i-- {
+			fmt.Sprintln(line[i])
+			val, err := strconv.Atoi(string(line[i]))
+			if err != nil {
+				continue
+			}
+			num += val
+			break
+		}
+		sum += num
+	}
+
+	fmt.Println(fmt.Sprintf(" =  %d", sum))
 }
